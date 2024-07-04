70f5766cb2696acbc777167f6e7e415a485d73e7
Mr <gotteshiva95@gmail.com>
2024-07-04 23:34:59 +0530

    updates

--- main.js
+++ main.js
@@ -2,21 +2,10 @@ const menu = document.getElementById('menu')
 const active = document.getElementById('active')
 const remove = document.getElementById('remove')
 const img1 = document.getElementById('profile1')
-const removeImg = document.getElementById('remove-img')
-const imageViewer = document.getElementById('image-viewer')
-
 
 menu.addEventListener('click',function(){
     active.style.display = 'flex'
 })
 remove.addEventListener('click',function(){
     active.style.display = 'none'
-})
-
-img1.addEventListener('click',function(){
-    imageViewer.style.display = 'flex'
-})
-
-removeImg.addEventListener('click',function(){
-    imageViewer.style.display = 'none';
 })
