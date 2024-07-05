15d705390bc5c86f976eb41c97ebf34efe304b6d
Mr <gotteshiva95@gmail.com>
2024-07-05 13:48:40 +0530

    changes

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const menu = document.getElementById('menu')
 const active = document.getElementById('active')
 const remove = document.getElementById('remove')
-const img1 = document.getElementById('profile1')
 
 menu.addEventListener('click',function(){
-    active.style.display = 'flex'
+    active.style.display = 'flex';
 })
 remove.addEventListener('click',function(){
-    active.style.display = 'none'
+    active.style.display = 'none';
+})
+active.addEventListener('click',function(){
+    active.style.display = 'none';
 })
